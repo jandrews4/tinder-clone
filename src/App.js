f40ffb6ec08928c25593eb29bf7aa87839c6fee5
jandrews4 <78866714+jandrews4@users.noreply.github.com>
2021-06-12 17:59:05 -0600

    links and buttons

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import './App.css';
 import Header from './Header';
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import TinderCards from './TinderCards';
+import SwipeButtons from './SwipeButtons';
 
 function App() {
   return (
     <div className="App">
       {/* header */}
-      <Header />
       <Router>
+        <Header />
         <Switch>
         <Route path="/chat">
           <h1>Chat page</h1>
         </Route>
         <Route path="/">
           <TinderCards/>
+          <SwipeButtons />
         </Route>
 
         </Switch>
--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 import PersonIcon from '@material-ui/icons/Person';
 import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
 import IconButton from '@material-ui/core/IconButton';
-
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
@@ -11,13 +11,17 @@ function Header() {
         <IconButton>=
         <PersonIcon className="header__icon" fontSize="large"/>
         </IconButton>
+        <Link to="/">
         <img 
         className="header__logo"
         src="https://logodix.com/logo/1986183.jpg" 
         alt="logo"/>
-        <IconButton>
-        <ChatBubbleIcon className="header__icon" fontSize="large"/>
-        </IconButton>
+        </Link>
+        <Link to="/chat">
+            <IconButton>
+                <ChatBubbleIcon className="header__icon" fontSize="large"/>
+            </IconButton>
+        </Link>
     </div>
     );
 }
